cc1166dde0556e6234c2f4f257f6de483d23961f
AlexanderSLutz <asl101499@gmail.com>
2021-11-04 10:27:49 -0500

    initial commit for thanks for submitting page

--- src/pages/ThanksForSubmitting.js
+++
@@ -1,4 +0,0 @@
-import * as React from "react"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
---
+++ src/pages/thanks-for-submitting.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const ThanksForSubmitting = () => (
+    <Layout>
+        <Seo title ="Thanks For Submitting" />
+        
+    </Layout>
+)
+
+export default ThanksForSubmitting
+
+
