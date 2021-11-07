ff839a9a03c59b171b3e13e518c0832edd8f84af
AlexanderSLutz <asl101499@gmail.com>
2021-11-06 20:28:55 -0500

    Added working thanks for submitting page as well as linked the page to the nav page for now until we figure everythign out.

--- src/pages/thanks-for-submitting.js
+++ src/pages/thanks-for-submitting.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const ThanksForSubmitting = () => (
+const ThanksForSubmitting = () => {
+    return(
     <Layout>
         <Seo title ="Thanks For Submitting" />
-        
+        <div>
+            <h1>Thanks For Submitting!</h1>
+        </div>
+        <Link to="/">Go back to the homepage</Link>
     </Layout>
-)
+    )
+}
+    
+
 
 export default ThanksForSubmitting
-
-
